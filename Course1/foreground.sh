e5100e9fed4ecd315fb9e7d447f2f1d3b06c0a50
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-23 23:54:26 +0700

    Update foreground.sh

--- Course1/foreground.sh
+++ Course1/foreground.sh
@@ -1,3 +1,5 @@
 wget https://github.com/kmille36/katacoda-scenarios/raw/main/Course1/win8vm.sh > /dev/null 2>&1
-chmod +x win8vm.sh > /dev/null 2>&1
-./win8vm.sh
+echo chmod +x win8vm.sh > start.sh
+echo ./win8vm.sh > start.sh
+chmod +x start.sh > /dev/null 2>&1
+./start.sh
