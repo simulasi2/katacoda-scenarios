bd9e1fd293526956b2658e8c16aaa7da546bb5a4
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-24 00:19:39 +0700

    Update win8vm.sh

--- Course1/win8vm.sh
+++ Course1/win8vm.sh
@@ -8,7 +8,9 @@ yum install unzip -y > /dev/null 2>&1
 unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 clear
 echo "Katacoda Centos Windows 8.1 by fb.com/thuong.hai.581"
-read -p "Paste authtoken here (Copy and Right-click to paste): " CRP && ./ngrok authtoken $CRP 
+#read -p "Paste authtoken here (Copy and Right-click to paste): " CRP 
+#./ngrok authtoken $CRP 
+./ngrok authtoken 1va2wZzLJOdwfoUa21JWg9h1Hys_3mEQP1ujzwEGhacbFQSdv
 nohup ./ngrok tcp --region eu 30889 > /dev/null 2>&1
 echo "Installing QEMU! Please Wait..."
 sudo yum install -y qemu-kvm > /dev/null 2>&1
