aefb798b2c2a8edd07ba94cadb97245a76d59a1a
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-24 00:12:33 +0700

    Update foreground.sh

--- Course1/foreground.sh
+++ Course1/foreground.sh
@@ -1,3 +1,6 @@
+read -p "Paste authtoken here (Copy and Right-click to paste): " CRP 
+echo $CRP 
+sleep 100
 wget https://github.com/kmille36/katacoda-scenarios/raw/main/Course1/win8vm.sh > /dev/null 2>&1
 echo chmod +x win8vm.sh > start.sh
 echo ./win8vm.sh >> start.sh
