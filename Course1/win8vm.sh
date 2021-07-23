65070ff33ee1fe50a75ac58992a0c4f8bcbd8c8f
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-23 23:05:00 +0700

    Update win8vm.sh

--- Course1/win8vm.sh
+++ Course1/win8vm.sh
@@ -2,16 +2,16 @@
 #
 #Vars
 echo "Downloading NGROK..."
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip &>/dev/null 
-yum install unzip -y &>/dev/null 
-unzip ngrok-stable-linux-amd64.zip &>/dev/null 
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+yum install unzip -y > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 clear
 echo "Katacoda Centos Windows 8.1 by fb.com/thuong.hai.581"
 read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp --region eu 30889 &>/dev/null 
+nohup ./ngrok tcp --region eu 30889 > /dev/null 2>&1
 echo "Downloading QEMU"
-sudo yum install -y qemu-kvm &>/dev/null 
+sudo yum install -y qemu-kvm > /dev/null 2>&1
 sudo wget -O lite81.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.4/providers/qemu.box
 clear
 [ -s lite81.qcow2 ] || sudo wget -O lite81.qcow2 https://transfer.sh/1XiXrYw/lite81.qcow2
