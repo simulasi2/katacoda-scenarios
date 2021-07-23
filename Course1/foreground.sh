0a0c65e8d5e075f54a61d1df9a4825cd2068c878
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-23 22:55:50 +0700

    Update foreground.sh

--- Course1/foreground.sh
+++ Course1/foreground.sh
@@ -1,4 +1,4 @@
 sudo su -
-wget -O Katacoda_Windows81SuperLite.sh https://bit.ly/3hUxv5T
+wget -O https://github.com/kmille36/katacoda-scenarios/raw/main/Course1/win8vm.sh
 chmod +x Katacoda_Windows81SuperLite.sh
 ./Katacoda_Windows81SuperLite.sh
