c959cde1b5bcd07098a4ba5e1ed7ffdcd8b98f07
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-24 17:36:53 +0700

    Update ngrok.sh

--- Course1/ngrok.sh
+++ Course1/ngrok.sh
@@ -19,7 +19,7 @@ nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 sleep 0.2
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels >/dev/null && bash win8vm.sh
 
-./ngrok authtoken 1va2wZzLJOdwfoUa21JWg9h1Hys_3mEQP1ujzwEGhacbFQSdv
+./ngrok authtoken 1va3C0pinwNxLotzdsVz2vqNxjZ_486E813nDK2hND7PgxLWv
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 sleep 0.2
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels >/dev/null && bash win8vm.sh || echo "All Tunnel Are In-Used"
