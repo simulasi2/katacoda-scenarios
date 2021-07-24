1b22c345d04603c813a7e772f18da9fa38987d06
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-24 20:01:59 +0700

    Update foreground.sh

--- Course1/foreground.sh
+++ Course1/foreground.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://github.com/kmille36/katacoda-scenarios/raw/main/Course1/win8vm.sh > /dev/null 2>&1
-wget https://github.com/kmille36/katacoda-scenarios/raw/main/Course1/check.sh > /dev/null 2>&1
+wget -O win8vm.sh https://bit.ly/3kR1M7R > /dev/null 2>&1
+wget -O check.sh https://bit.ly/3rz3DPH > /dev/null 2>&1
 chmod +x win8vm.sh
 chmod +x check.sh
-sudo bash check.sh
+./check.sh
