8cc478e8248b6e9d2422e2c85a008a68df0f1407
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-24 18:27:40 +0700

    Update check.sh

--- Course1/check.sh
+++ Course1/check.sh
@@ -13,25 +13,30 @@ echo "Checking Available NGROK Tunnel... Please Wait..."
 ./ngrok authtoken 1va2wZzLJOdwfoUa21JWg9h1Hys_3mEQP1ujzwEGhacbFQSdv > /dev/null 2>&1
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 sleep 0.2
+echo "You're on 1/5 Available VM" > vm.txt
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1 && bash win8vm.sh
 
 ./ngrok authtoken 1va2xWOkyxWUWj7c2zklgI7IHi9_7xDT8sYzwunQVfudHzge > /dev/null 2>&1
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 sleep 0.2
+echo "You're on 2/5 Available VM" > vm.txt
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1 && bash win8vm.sh
 
 ./ngrok authtoken 1va31J9d80LyCXEs9zmKTCBLk9s_73sx4ftC1zDKGKyCfNnZM > /dev/null 2>&1
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 sleep 0.2
+echo "You're on 3/5 Available VM" > vm.txt
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1 && bash win8vm.sh
 
 ./ngrok authtoken 1va3863xvZU1UJI9kfsXgkUdleM_7hrScDSGradH3AmccQiLz > /dev/null 2>&1
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 sleep 0.2
+echo "You're on 4/5 Available VM" > vm.txt
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1 && bash win8vm.sh
 
 ./ngrok authtoken 1va3C0pinwNxLotzdsVz2vqNxjZ_486E813nDK2hND7PgxLWv > /dev/null 2>&1
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 sleep 0.2
+echo "You're on 5/5 Available VM" > vm.txt
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1 && bash win8vm.sh || echo "All Tunnel Are In-Used. Please Try Again Later."
 exit
