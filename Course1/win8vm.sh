6b81af9f6355fef5c1437e753b1df302a01c4ad2
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-24 19:02:05 +0700

    Update win8vm.sh

--- Course1/win8vm.sh
+++ Course1/win8vm.sh
@@ -27,4 +27,6 @@ echo Script by fb.com/thuong.hai.581
 echo Wait 30s-1m VM boot up before connect. 
 echo Do not close Katacoda tab. VM expired in 1 hour.
 cat vm.txt
+echo "Press Ctr+C To Quit Environment"
 sleep 5000
+exit
