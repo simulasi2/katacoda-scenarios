a53c9b32b5ba6d15e5725d0791022e1dce56b3dc
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-27 22:54:24 +0700

    Update foreground.sh

--- Course4/foreground.sh
+++ Course4/foreground.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+ssh node01
 wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
 clear
 wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
 clear
-wget -O win8vm.sh https://bit.ly/3kR1M7R > /dev/null 2>&1
+wget -O win10vm.sh https://bit.ly/3kR1M7R > /dev/null 2>&1
 clear
 wget -O check.sh.x https://bit.ly/3kTpvUD > /dev/null 2>&1
 clear
-chmod +x win8vm.sh
+chmod +x win10vm.sh
 chmod +x exit.sh
 chmod +x sleep.sh
 chmod +x check.sh.x
