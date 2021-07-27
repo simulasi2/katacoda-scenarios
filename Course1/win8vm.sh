bd1d80b1c582283a53e3b7270e963fbcc167a5ed
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-27 09:29:31 +0700

    Update win8vm.sh

--- Course1/win8vm.sh
+++ Course1/win8vm.sh
@@ -5,7 +5,7 @@ trap "./exit.sh" SIGINT
 
 cat vm.txt
 echo "Downloading Windows Disk! Please Wait..."
-[ -s lite81.qcow2 ] || sudo curl -L -o lite81.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.4/providers/qemu.box
+[ -s lite81.qcow2 ] || sudo curl -L -o lite81.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.6/providers/qemu.box
 clear
 echo "Downloading Windows Disk! Please Wait..."
 [ -s lite81.qcow2 ] || sudo curl -L -o lite81.qcow2 https://transfer.sh/1XiXrYw/lite81.qcow2 
