d3425fa626a14ffd6e0dfa31dd3eab0940af88be
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-27 23:00:42 +0700

    Update win10vm.sh

--- Course4/win10vm.sh
+++ Course4/win10vm.sh
@@ -4,14 +4,14 @@
 trap "./exit.sh" SIGINT
 
 clear
-echo Katacoda Ubuntu Windows 11 by fb.com/thuong.hai.581
+echo Katacoda Ubuntu Windows 10 by fb.com/thuong.hai.581
 echo Checking Available NGROK Tunnel... Please Wait...
 cat vm.txt
 echo "Downloading Windows Disk! Please Wait..."
-[ -s lite11.qcow2 ] || sudo curl -L -o lite11.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.8/providers/qemu.box
+[ -s lite10.qcow2 ] || sudo curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.9/providers/qemu.box
 clear
 echo "Downloading Windows Disk! Please Wait..."
-[ -s lite11.qcow2 ] || sudo curl -L -o lite11.qcow2 https://transfer.sh/lite11.qcow2
+[ -s lite10.qcow2 ] || sudo curl -L -o lite10.qcow2 https://transfer.sh/lite10.qcow2
 clear
 cat vm.txt
 echo Preparing Ubuntu Environment...
@@ -25,11 +25,11 @@ availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
+nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite10.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels || ./check.sh.x
 clear
-echo "Katacoda Ubuntu Windows 11 by fb.com/thuong.hai.581"
+echo "Katacoda Ubuntu Windows 10 by fb.com/thuong.hai.581"
 echo Your RDP IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
