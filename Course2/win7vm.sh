6a0a30015c58bf7f8a5a3ebec308b6c001ae407d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-27 10:10:04 +0700

    Update win7vm.sh

--- Course2/win7vm.sh
+++ Course2/win7vm.sh
@@ -3,25 +3,33 @@
 #Vars
 trap "./exit.sh" SIGINT
 
+clear
+Katacoda Centos Windows 7 by fb.com/thuong.hai.581
+Checking Available NGROK Tunnel... Please Wait...
 cat vm.txt
+echo "Downloading Windows Disk! Please Wait..."
+[ -s lite7.qcow2 ] || sudo curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.7/providers/qemu.box
+clear
+echo "Downloading Windows Disk! Please Wait..."
+[ -s lite7.qcow2 ] || sudo curl -L -o lite7.qcow2 https://transfer.sh/11aPVVu/lite7.qcow2
+clear
+cat vm.txt
+echo Preparing Ubuntu Environment...
+sudo killall apt apt-get > /dev/null 2>&1
+dpkg --configure -a > /dev/null 2>&1
+sudo apt-get update -y > /dev/null 2>&1
 echo "Installing QEMU! Please Wait..."
-sudo yum install -y qemu-kvm > /dev/null 2>&1
-clear
-echo "Downloading Windows Disk! Please Wait..."
-[ -s lite7.qcow2 ] || sudo wget -O lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.3/providers/qemu.box --progress=bar:force 2>&1 | tail -f -n +6
-clear
-echo "Downloading Windows Disk! Please Wait..."
-[ -s lite7.qcow2 ] || sudo wget -O lite7.qcow2 https://transfer.sh/1imYuZo/lite7.qcow2 --progress=bar:force 2>&1 | tail -f -n +6
+sudo apt-get install -y qemu-kvm > /dev/null 2>&1
 clear
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
-custom_param_ram="-m "$(expr $availableRAM - 856 )"M"
+custom_param_ram="-m "$(expr $availableRAM)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite7.qcow2,index=0,media=disk,format=qcow2,if=virtio -boot once=d &>/dev/null &
+nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite7.qcow2,index=0,media=disk,format=qcow2,if=virtio -boot once=d &>/dev/null &
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels || ./check.sh.x
 clear
-echo "Katacoda Centos Windows 8.1 by fb.com/thuong.hai.581"
+echo "Katacoda Ubuntu Windows 7 by fb.com/thuong.hai.581"
 echo Your RDP IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
