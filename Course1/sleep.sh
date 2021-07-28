38d9e9b60086dff614c38747fc6cf3d16ad38c13
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-28 23:01:33 +0700

    Update sleep.sh

--- Course1/sleep.sh
+++ Course1/sleep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 trap "./exit.sh" SIGINT
-trap "./region.sh" SIGQUIT
+trap "./region.sh" exit
 echo "To Quit Environment Press Ctrl+C"
 echo "To Change Ngrok Region Press Ctrl+D"
 sleep 5000
