025ce8fa2dc063a332bfba66721bf1cdae0522e0
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 01:17:51 +0700

    Update region.sh

--- Course4/region.sh
+++ Course4/region.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo Katacoda Ubuntu Windows 8.1 by fb.com/thuong.hai.581
+echo Katacoda Ubuntu Windows 10 by fb.com/thuong.hai.581
 
 PS3='Choose your NGROK REGION you want to change (type 1, 2, 3, 4, 5, 6 then Enter): '
 foods=("US" "AP" "AU" "SA" "JP" "IN")
@@ -41,6 +41,6 @@ select fav in "${foods[@]}"; do
 done
 
 echo Change Ngrok Region Success!!!
-./win8vm.sh
+./win10vm.sh
 
 
