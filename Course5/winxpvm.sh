f96cda42c275f452b5295a3058cc9711038f9ee9
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 14:00:06 +0700

    Update winxpvm.sh

--- Course5/winxpvm.sh
+++ Course5/winxpvm.sh
@@ -27,8 +27,8 @@ availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=xpsp3.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
-[ -s check.txt ] || echo "Waiting for windows XP setting up (90s)..."
-./spinner.sh sleep 90
+[ -s check.txt ] || echo "Waiting for windows XP setting up (80s)..."
+./spinner.sh sleep 80
 echo check done > check.txt
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels || ./check.sh.x
