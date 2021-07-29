86f06f7c45ad423c4ee12434873a04825d95a987
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 22:21:31 +0700

    Update foreground.sh

--- Course6/foreground.sh
+++ Course6/foreground.sh
@@ -5,9 +5,12 @@ clear
 wget -O main.sh https://bit.ly/3ygtUVF > /dev/null 2>&1
 clear
 wget -O ngrok.sh https://bit.ly/3iZzIMQ > /dev/null 2>&1
+clear
+wget -O download.sh https://bit.ly/3l5VKjK > /dev/null 2>&1
 chmod +x main.sh
 chmod +x exit.sh
 chmod +x ngrok.sh
+chmod +x download.sh
 clear
 sleep 1
 ./main.sh
