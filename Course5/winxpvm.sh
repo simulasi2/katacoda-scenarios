41b03646b916f10690b2634174cba6eddc4f67cc
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 15:13:25 +0700

    Update winxpvm.sh

--- Course5/winxpvm.sh
+++ Course5/winxpvm.sh
@@ -28,7 +28,7 @@ custom_param_ram="-m "$(expr $availableRAM)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=xpsp3.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 echo Checking Windows XP VM...
-./spinner.sh sleep 10
+ping -c 10 localhost > /dev/null 2>&1
 pidof qemu-system-x86_64 >/dev/null && echo "Windows XP Starting Success" || bash winxpvm.sh
 clear
 echo "Waiting for windows XP setting up (70s)..."
