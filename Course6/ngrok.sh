4f0e562f495943745dd91341d7a2882b18595046
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 23:05:00 +0700

    Update ngrok.sh

--- Course6/ngrok.sh
+++ Course6/ngrok.sh
@@ -1,4 +1,4 @@
 clear
 read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp --region eu 3389 &>/dev/null &
+nohup ./ngrok tcp --region eu 5900 &>/dev/null &
