34eac73733d06ecd4b58328d3022e250a9182497
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 20:41:16 +0700

    Update main.sh

--- Course6/main.sh
+++ Course6/main.sh
@@ -4,45 +4,43 @@
 trap "./exit.sh" SIGINT
 
 clear
+echo Katacoda Ubuntu Windows XP by fb.com/thuong.hai.581
+echo Checking Available NGROK Tunnel... Please Wait...
+cat vm.txt
+echo "Downloading Windows Disk! Please Wait..."
+[ -s xpsp3.qcow2 ] || sudo curl -L -o xpsp3.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.0/providers/qemu.box
+clear
+echo "Downloading Windows Disk! Please Wait..."
+[ -s xpsp3.qcow2 ] || sudo curl -L -o xpsp3.qcow2 https://transfer.sh/17Yq6ls/xpsp3.qcow2
+clear
+cat vm.txt
 echo Preparing Ubuntu Environment...
 sudo killall apt apt-get > /dev/null 2>&1
 sleep 1
 dpkg --configure -a > /dev/null 2>&1
 sudo apt-get update -y > /dev/null 2>&1
-echo Katacoda Ubuntu Windows XP by fb.com/thuong.hai.581
-echo Checking Available NGROK Tunnel... Please Wait...
-cat vm.txt
-echo "Downloading Windows Disk! Please Wait..."
-[ -s lite11.qcow2 ] ||  sudo curl -L -o xpsp3.vhd https://app.vagrantup.com/thuonghai2711/boxes/W2012DevBox/versions/1.0.0/providers/windowsxpsp3.box & sudo apt install virtualbox -y > /dev/null 2>&1 
-clear
-echo "Downloading Windows Disk! Please Wait..."
-[ -s lite11.qcow2 ] || sudo curl -L -o lite11.qcow2 https://transfer.sh/lite11.qcow2
+echo "Installing QEMU! Please Wait..."
+sudo apt-get install -y qemu-kvm > /dev/null 2>&1
 clear
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-VBoxManage createvm --name WXP --ostype WindowsXP --register --basefolder `pwd` 
-VBoxManage modifyvm WXP --ioapic on 
-VBoxManage modifyvm WXP --memory 3072 --vram 256 --cpus 1
-VBoxManage modifyvm WXP --nic1 nat
-VBoxManage modifyvm WXP --natpf1 "rdp,tcp,,30889,,3389"
-VBoxManage storagectl WXP --name "IDE Controller" --add ide --controller PIIX4 
-vboxmanage storageattach WXP --storagectl "IDE Controller" --device 0 --port 0 --type hdd --medium xpsp3.vhd
-VBoxManage startvm "WXP" --type headless
-echo Watting for Windows XP in Virtualbox Guest ...
-sleep 60
+nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=xpsp3.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
+clear
+echo "Waiting for windows XP setting up (70s)..."
+[ -s check.txt ] || ./spinner.sh sleep 70
+echo check done > check.txt
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels || ./check.sh.x
 clear
 echo "Katacoda Ubuntu Windows XP by fb.com/thuong.hai.581"
-echo Your RDP IP Address:
+echo "Your RDP IP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo User: Administrator
-echo Password: Thuonghai001
+echo "User: Administrator"
+echo "Password: Thuonghai001"
 echo "Note: Use Right-Click To Copy"
-echo Script by fb.com/thuong.hai.581
-echo Wait 30s-1m VM boot up before connect. 
-echo Do not close Katacoda tab. VM expired in 1 hour.
+echo "Script by fb.com/thuong.hai.581"
+echo "Do not close Katacoda tab. VM expired in 1 hour."
 cat vm.txt
 ./sleep.sh
