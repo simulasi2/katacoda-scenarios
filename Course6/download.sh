019765461003ee141ba5556ee51ad3ed3f108934
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 22:28:17 +0700

    Update download.sh

--- Course6/download.sh
+++ Course6/download.sh
@@ -6,15 +6,14 @@ foods=("Direct-Link" "Google-Drive")
 select fav in "${foods[@]}"; do
     case $fav in
         "Direct-Link")
-            windows_os_link=https://app.vagrantup.com/thuonghai2711/boxes/WindowsIMG/versions/1.0.2/providers/qemu.box
-            windows_os_name="Windows Server 2012 R2"
-	    custom_param_disk="windows2012.raw"
+            read -p "Paste direct iso link here (Copy and Right-click to paste): " ISO
+            wget -O windows.iso $ISO
             break
             ;;
         "Google-Drive")
-            windows_os_link=https://app.vagrantup.com/thuonghai2711/boxes/WindowsIMG/versions/1.0.0/providers/qemu.box
-            windows_os_name="Windows Server 2022 Preview"
-	    custom_param_disk="windows2022.img"
+	    read -p "Paste your FileID in Google Drive here (Copy and Right-click to paste): " ID
+	    gdrive download $ID
+	    mv *.iso windows.iso
             break
             ;;
         *) echo "invalid option $REPLY";;
