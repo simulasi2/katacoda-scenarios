47671cef5d13be048fafe631d7fdb42c1ae729e2
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 23:54:00 +0700

    Update upload.sh

--- Course6/upload.sh
+++ Course6/upload.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+clear
 
+PS3='Choose your GZ Upload Method you want to (type 1, 2 Enter): '
+foods=("Transfer-sh" "Google-Drive")
+select fav in "${foods[@]}"; do
+    case $fav in
+        "Transfer-sh")
+            echo "Your GZ file are uploading... Take up to 5-10m."
+            curl --upload-file ./WindowsCustom.gz https://transfer.sh/WindowsCustom.gz
+            break
+            ;;
+        "Google-Drive")
+            gdrive upload WindowsCustom.gz
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
