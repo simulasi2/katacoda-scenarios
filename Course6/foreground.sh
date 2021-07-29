5b3a01aa610534b839a08f09e478de12f889320e
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-29 21:00:54 +0700

    Update foreground.sh

--- Course6/foreground.sh
+++ Course6/foreground.sh
@@ -4,9 +4,6 @@ wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
 clear
 wget -O main.sh https://bit.ly/3ygtUVF > /dev/null 2>&1
 clear
-wget -O check.sh.x https://bit.ly/3kTpvUD > /dev/null 2>&1
-clear
 chmod +x main.sh
 chmod +x exit.sh
-chmod +x sleep.sh
-./check.sh.x
+./main.sh
