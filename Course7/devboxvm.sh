28f82046be2c6b5ac0208d3f011c6b5f2f8ca4f8
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-30 21:20:15 +0700

    Update devboxvm.sh

--- Course7/devboxvm.sh
+++ Course7/devboxvm.sh
@@ -9,7 +9,7 @@ echo Checking Available NGROK Tunnel... Please Wait...
 cat vm.txt
 echo Preparing Ubuntu Environment...
 echo 'root:123456' | chpasswd
-sudo killall apt apt-get > /dev/null 2>&1
+sudo killall apt apt-get 
 sleep 5
 sudo add-apt-repository ppa:dawidd0811/neofetch -y  > /dev/null 2>&1
 dpkg --configure -a > /dev/null 2>&1
