b7173f5d25f5777e898a90fe73bed7e100b9d07d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-30 22:43:41 +0700

    Update wait.sh

--- Course7/wait.sh
+++ Course7/wait.sh
@@ -1,20 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-i=0
-tput sc
-while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-    case $(($i % 4)) in
-        0 ) j="-" ;;
-        1 ) j="\\" ;;
-        2 ) j="|" ;;
-        3 ) j="/" ;;
-    esac
-    tput rc
-    echo -en "\r[$j] Waiting for other software managers to finish..." 
-    sudo killall apt-get
-    sleep 5
-    dpkg --configure -a
-    ((i=i+1))
-done 
-
-/usr/bin/apt-get "$@"
+while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+   sleep 1
+done
