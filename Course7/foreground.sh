950fe255ae6fc870d18e8e077c450b16d6b5faee
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 00:15:55 +0700

    Update foreground.sh

--- Course7/foreground.sh
+++ Course7/foreground.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 ssh node01
-sleep 1
 wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
-clear
 wget -O sleep.sh https://bit.ly/376kyA1 > /dev/null 2>&1
-clear
 wget -O devboxvm.sh https://bit.ly/37e7QyU > /dev/null 2>&1
-clear
 wget -O region.sh https://bit.ly/3rJD2zR > /dev/null 2>&1
-clear
 wget -O check.sh.x https://bit.ly/3zNy1sL > /dev/null 2>&1
-clear
 wget -O wait.sh https://bit.ly/3zV1jFE > /dev/null 2>&1
 chmod +x devboxvm.sh
 chmod +x exit.sh
@@ -18,6 +12,5 @@ chmod +x sleep.sh
 chmod +x region.sh
 chmod +x wait.sh
 chmod +x check.sh.x
-clear
 sleep 1
 ./check.sh.x
