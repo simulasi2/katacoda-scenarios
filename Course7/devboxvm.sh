8daab4880ddc36ead2e6bf2c9eb3377cc05a3d2b
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-30 16:34:49 +0700

    Update devboxvm.sh

--- Course7/devboxvm.sh
+++ Course7/devboxvm.sh
@@ -7,13 +7,6 @@ clear
 echo Katacoda Ubuntu 16.04 LTS by fb.com/thuong.hai.581
 echo Checking Available NGROK Tunnel... Please Wait...
 cat vm.txt
-echo "Downloading Windows Disk! Please Wait..."
-[ -s xpsp3.qcow2 ] || sudo curl -L -o xpsp3.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.0/providers/qemu.box
-clear
-echo "Downloading Windows Disk! Please Wait..."
-[ -s xpsp3.qcow2 ] || sudo curl -L -o xpsp3.qcow2 https://transfer.sh/17Yq6ls/xpsp3.qcow2
-clear
-cat vm.txt
 echo Preparing Ubuntu Environment...
 sudo killall apt apt-get > /dev/null 2>&1
 sleep 1
@@ -21,15 +14,10 @@ dpkg --configure -a > /dev/null 2>&1
 sudo apt-get update -y > /dev/null 2>&1
 echo "Installing QEMU! Please Wait..."
 sudo apt-get install -y qemu-kvm > /dev/null 2>&1
-clear
-availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
-availableRAM=$(echo $availableRAMcommand | bash)
-custom_param_ram="-m "$(expr $availableRAM)"M"
-cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=xpsp3.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
-clear
-echo "Waiting for windows XP setting up (70s)..."
-[ -s check.txt ] || ./spinner.sh sleep 70
+echo "Installing VirtualBox! Please Wait..."
+sudo apt-get install virtualbox -y > /dev/null 2>&1
+echo "Installing XFCE4 Destop Environment..."
+sudo apt-get install firefox xfonts-base xfce4 xfce4-goodies > /dev/null 2>&1
 echo check done > check.txt
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels || ./check.sh.x
