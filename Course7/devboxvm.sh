f3ee0b8bdc117aaa04a9cc64b039d4117a3aea2e
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-30 21:55:15 +0700

    Update devboxvm.sh

--- Course7/devboxvm.sh
+++ Course7/devboxvm.sh
@@ -9,11 +9,8 @@ echo Checking Available NGROK Tunnel... Please Wait...
 cat vm.txt
 echo Preparing Ubuntu Environment...
 echo 'root:123456' | chpasswd
-##sudo killall apt apt-get > /dev/null 2>&1
-sleep 10
 sudo add-apt-repository ppa:dawidd0811/neofetch -y  > /dev/null 2>&1
-dpkg --configure -a > /dev/null 2>&1
-sudo apt-get update -y > /dev/null 2>&1
+./wait.sh update -y 
 sudo apt install neofetch -y  > /dev/null 2>&1
 echo "Installing XFCE4 Desktop Environment..."
 sudo apt-get install -y firefox xfonts-base xfce4 xfce4-goodies xrdp > /dev/null 2>&1
