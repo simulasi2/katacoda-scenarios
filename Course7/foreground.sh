6849355c79d7b6ec379b2833a6f3e2b96ce33d34
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 17:12:14 +0700

    Update foreground.sh

--- Course7/foreground.sh
+++ Course7/foreground.sh
@@ -2,10 +2,12 @@
 
 
 
-if [ "$HOSTNAME" = controlplane ]; then
+if [ "$HOSTNAME" = controlplane1 ]; then
     while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; scp -r /usr/local/bin/wait.sh node01:/usr/local/bin/
     ssh node01 
-    wait.sh
+
 else
-    printf '%s\n' "uh-oh, not on foo"
+    sleep 999999999999
 fi
+  
+wait.sh
