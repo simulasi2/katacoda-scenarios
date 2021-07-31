f211aeb254cc033adfdbf483cc66e8e37f01c65c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 18:32:30 +0700

    Update wait.sh

--- Course5/assets/wait.sh
+++ Course5/assets/wait.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
 wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
 wget -O winxpvm.sh https://bit.ly/3BNeNFj > /dev/null 2>&1
