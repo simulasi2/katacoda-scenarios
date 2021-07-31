27990e9a0863e49362c715fb8d5db3568ea99e28
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 19:23:21 +0700

    Update wait.sh

--- Course6/assets/wait.sh
+++ Course6/assets/wait.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
+wget -O main.sh https://bit.ly/3ygtUVF > /dev/null 2>&1
+wget -O ngrok.sh https://bit.ly/3iZzIMQ > /dev/null 2>&1
+wget -O download.sh https://bit.ly/3l5VKjK > /dev/null 2>&1
+wget -O upload.sh https://bit.ly/3zQ3yKq > /dev/null 2>&1
+chmod +x main.sh
+chmod +x exit.sh
+chmod +x ngrok.sh
+chmod +x download.sh
+chmod +x upload.sh
+./main.sh
