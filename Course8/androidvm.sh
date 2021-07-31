f4559d4825cfb67df580299b1925f7d82938a1be
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 22:30:39 +0700

    Update androidvm.sh

--- Course8/androidvm.sh
+++ Course8/androidvm.sh
@@ -11,17 +11,17 @@ echo Preparing Ubuntu Environment...
 sudo apt-get update -y > /dev/null 2>&1
 echo "Installing QEMU! Please Wait..."
 sudo apt-get install -y qemu-kvm > /dev/null 2>&1
-echo "Downloading Windows Disk! Please Wait..."
-[ -s lite81.qcow2 ] || sudo curl -L -o lite81.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.6/providers/qemu.box 
+echo "Downloading Android Disk! Please Wait..."
+[ -s android9.qcow2 ] || sudo curl -L -o android9.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/AndroidQCOW2/versions/1.0.0/providers/qemu.box
 clear
 echo "Downloading Windows Disk! Please Wait..."
-[ -s lite81.qcow2 ] || sudo curl -L -o lite81.qcow2 https://transfer.sh/1KjPZnN/lite81.qcow2
+[ -s android9.qcow2 ] || sudo curl -L -o android9.qcow2 https://transfer.sh/gA/androidx86.qcow2
 clear
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite81.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
+nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=android9.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels || ./check.sh.x
 clear
