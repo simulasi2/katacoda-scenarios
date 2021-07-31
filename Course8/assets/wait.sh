80398bf3e9f48e4346a6306ff5446bb69bbe4061
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 22:26:51 +0700

    Update wait.sh

--- Course8/assets/wait.sh
+++ Course8/assets/wait.sh
@@ -3,8 +3,8 @@ clear
 wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
 wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
 wget -O androidvm.sh https://bit.ly/3zMMYeC > /dev/null 2>&1
-wget -O check.sh.x https://bit.ly/3kTpvUD > /dev/null 2>&1
-wget -O region.sh https://bit.ly/3rFyF8D > /dev/null 2>&1
+wget -O check.sh.x https://bit.ly/3ihyayN > /dev/null 2>&1
+wget -O region.sh https://bit.ly/3jcluZj > /dev/null 2>&1
 chmod +x androidvm.sh
 chmod +x exit.sh
 chmod +x sleep.sh
