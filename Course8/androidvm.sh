ec94a7ad3e360a586ace6d07ce1021cb629144b1
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 22:32:52 +0700

    Update androidvm.sh

--- Course8/androidvm.sh
+++ Course8/androidvm.sh
@@ -4,7 +4,7 @@
 trap "./exit.sh" SIGINT
 
 clear
-echo Katacoda Ubuntu Windows 8.1 by fb.com/thuong.hai.581
+echo Katacoda Ubuntu Android 9.0 by fb.com/thuong.hai.581
 echo Checking Available NGROK Tunnel... Please Wait...
 cat vm.txt
 echo Preparing Ubuntu Environment...
@@ -21,11 +21,11 @@ availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=android9.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
+nohup sudo qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=android9.qcow2,index=0,media=disk,format=qcow2 -boot once=d -vnc :0 &>/dev/null &
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels || ./check.sh.x
 clear
-echo "Katacoda Ubuntu Windows 8.1 by fb.com/thuong.hai.581"
+echo "Katacoda Ubuntu Android 9.0 by fb.com/thuong.hai.581"
 echo Your RDP IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
