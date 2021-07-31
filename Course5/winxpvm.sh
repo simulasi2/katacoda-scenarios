bf98fac3f4490b8334c23e16d11ee1a0a387f33d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 18:05:17 +0700

    Update winxpvm.sh

--- Course5/winxpvm.sh
+++ Course5/winxpvm.sh
@@ -15,9 +15,6 @@ echo "Downloading Windows Disk! Please Wait..."
 clear
 cat vm.txt
 echo Preparing Ubuntu Environment...
-sudo killall apt apt-get > /dev/null 2>&1
-sleep 1
-dpkg --configure -a > /dev/null 2>&1
 sudo apt-get update -y > /dev/null 2>&1
 echo "Installing QEMU! Please Wait..."
 sudo apt-get install -y qemu-kvm > /dev/null 2>&1
